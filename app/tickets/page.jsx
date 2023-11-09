f59a3b1b2bc4a9812848662500c893ec2b85ca1e
Steve Culverwell <steve.culverwell@bbc.co.uk>
2023-11-09 22:13:15 +0000

    read from local db

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,5 +1,14 @@
+import TicketList from "./TicketList"
+
 export default function Tickets() {
   return (
-    <div>Tickets</div>
+    <main>
+      <nav>
+        <h2>Tickets</h2>
+        <p><small>Open tickets</small></p>
+      </nav>
+      <TicketList />
+    </main>
+    
   )
 }
