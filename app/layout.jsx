eabbc01be172f521e89d3f0d81f39e8f88fcb117
Steve Culverwell <steve.culverwell@bbc.co.uk>
2023-11-09 12:04:08 +0000

    adding to pages

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+
 
 // components
 import Navbar from './components/Navbar'
@@ -16,7 +18,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}</body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
